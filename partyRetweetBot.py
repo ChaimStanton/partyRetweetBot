a393eff8e51fb1f2c6f54b41cb7a1c88719825a5
Chaim Stanton <chaimstanton1@gmail.com>
2020-07-02 22:52:25 +0100

    writies the .gitiginore to a file

--- partyRetweetBot.py
+++ partyRetweetBot.py
@@ -10,14 +10,22 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-since_id = 1273073579786211330
+# since_id = 1273073579786211330
+since_id = 1
+users = [1268410636553371648]
 
 while True:
-    print ("NEW WHILE")
-    result = api.user_timeline(1268410636553371648, since_id=(since_id))
-    for tweet in result:
-        print("NEW FOR")
-        print(tweet.text, tweet.id)
-        # tweet.retweet()
-    since_id = result.since_id
-    sleep(5)
+    print ("NEW loop for program")
+    for user in users:
+        result = api.user_timeline(user, since_id=(since_id))
+        for tweet in result:
+            print("NEW for loop for the result")
+            print(tweet.text, tweet.id) # this prints it out for debugging purposes
+            # tweet.retweet()
+        if result.since_id == None: #bc if nothing gets tweeted then it returns none
+            pass 
+        else:
+            since_id = result.since_id
+            with open("since_id.txt", "w") as txtFile:
+                txtFile.write(str(since_id))
+        sleep(5)
