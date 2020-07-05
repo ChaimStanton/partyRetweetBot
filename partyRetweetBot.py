4ad5e5a3945fad35769178994b7f3ddfa45ba096
Chaim Stanton <38401049+ChaimStanton@users.noreply.github.com>
2020-07-05 03:41:36 +0100

    pep8

--- partyRetweetBot.py
+++ partyRetweetBot.py
@@ -2,30 +2,20 @@
 
 from keysAuthentication import *
 
-import requests
-import urllib3
-
 import tweepy
 from time import sleep
 
-# "new" authenticaiotn
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
-users = ["1268410636553371648"]
 
 def does_have_value(jsonObject, keyvalue):
     try:
         jsonObject[keyvalue]
         return True
     except KeyError:
-        return False 
+        return False
 
-# --- USING THE STREAMING METHOD --- 
-# class for the stream 
+
 class MyStreamListener(tweepy.StreamListener):
+    """This is a class for the stream """
     def on_status(self, status):
         tweet = Tweet(status)
         try:
@@ -39,20 +29,30 @@ class MyStreamListener(tweepy.StreamListener):
         print(status_code)
         return False
 
+
 class Tweet():
     """ A class for all of the tweets to make life easier """
     def __init__(self, statusObj):
         self.id = statusObj.__dict__["_json"]["id"]
-        self.isPureRetweet = does_have_value(status._json, "retweeted_status") # a pure retweet is a retweet with no comment
+        self.isPureRetweet = does_have_value(status._json, "retweeted_status")
+        # a pure retweet is a retweet with no comment
 
     def retweet(self):
         api.retweet(self.id)
 
-#creating and initialising the stream
+
+# "new" authenticaiotn
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+users = ["1268410636553371648"]
+
+# creating and initializing the stream
 myStreamListener = MyStreamListener()
 
-myStream = tweepy.Stream(auth= auth, listener=myStreamListener)
+myStream = tweepy.Stream(auth=auth, listener=myStreamListener)
 
 # starting the stream
 myStream.filter(follow=users)
-
