2073ef26d8a3a6c35b3e3c1f7519469e0cad8ee3
Chaim Stanton <chaimstanton1@gmail.com>
2020-07-05 03:23:27 +0100

    removed the wrong auth bit

--- partyRetweetBot.py
+++ partyRetweetBot.py
@@ -14,39 +14,6 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-"""
-# Auth 1 authenticaton
-try:
-    redirect_url = auth.get_authorization_url()
-except tweepy.TweepError:
-    print('Error! Failed to get request token.')
-
-print("go to this link", redirect_url)
-
-verifier = int(input('Verifier:'))
-
-
-s = requests.Session()
-# s.set("a")
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-token = s.get(redirect_url)
-s.delete(redirect_url)
-# auth.request_token = { 'oauth_token' : token,
-#                          'oauth_token_secret' : verifier }
-
-auth.request_token = {
-      'oauth_token': s.get(token.url),
-      'oauth_token_secret': verifier
-}
-
-# try:
-auth.get_access_token(str(verifier))
-print("yay")
-# except tweepy.TweepError:
-#     print('Error! Failed to get access token.')
-"""
-
 users = ["1268410636553371648"]
 
 # --- USING THE STREAMING METHOD --- 
